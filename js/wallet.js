34661f1702f62d6101ab8e55df202f6e67ef894c
dean840513 <50709683+dean840513@users.noreply.github.com>
2025-05-14 12:37:13 +0800

    Update wallet.js

--- js/wallet.js
+++ js/wallet.js
@@ -5,13 +5,13 @@ const isLocal = hostname === 'localhost' || hostname === '127.0.0.1' || hostname
 if (isLocal) {
   console.log('🌟 本地环境');
   // Anvil
-  const marketplaceAddress = "0xBc65508443bE8008Cf5af3973CCeF97F1Ea8888d";
-  const tatTokenAddress = "0xE41c36a93D60cD01CE8D17EB93CD4579ac6288D0";
+  marketplaceAddress = "0xBc65508443bE8008Cf5af3973CCeF97F1Ea8888d";
+  tatTokenAddress = "0xE41c36a93D60cD01CE8D17EB93CD4579ac6288D0";
 } else if (hostname.includes('github.io')) {
   console.log('🚀 GitHub Pages环境');
   //Polygon
-  const marketplaceAddress = "0x82aC52E1138344486C61C85697E8814a10060b23";
-  const tatTokenAddress = "0xEd3D92C6023516F33E8CEF41C7a583E4Ba5F23ce";
+  marketplaceAddress = "0x82aC52E1138344486C61C85697E8814a10060b23";
+  tatTokenAddress = "0xEd3D92C6023516F33E8CEF41C7a583E4Ba5F23ce";
 } else {
   console.log('🔍 其他环境（比如正式自定义域名部署）');
 }
